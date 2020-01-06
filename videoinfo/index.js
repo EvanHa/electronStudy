d733389c29a1de63294affe8e9b3933775984fa4
myunghwan <0x4861@neotrilabs.com>
2020-01-06 17:25:49 +0900

    [Add] support ipc between electron and mainwindow

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,10 +1,25 @@
 const electron = require('electron');
+//import electorn from 'electorn'; // es6
+const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, ipcMain } = electron;
 
-//import electorn from 'electorn';
-
-const { app, BrowserWindow } = electron;
+let mainWindow;
 
 app.on ('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true,
+        allowEval: false
+    }
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video duration is: ', metadata.format.duration);
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration);
+  });
 });
