866c852987f248b8ae789db98553e15f716b0687
myunghwan <myunghwan@hdac.io>
2021-02-25 20:38:54 +0900

    [Mod] part2

--- crawler/src/js/instagram_follower.js
+++ crawler/src/js/instagram_follower.js
@@ -9,7 +9,7 @@ let env = dotenv.config({path: env_path});
 
 async function instagram_follower_crawler(id, password) {
     try {
-        console.log('root_path : '+ root_path);
+        //console.log('root_path : '+ root_path);
         const browser = await puppeteer.launch({
             ignoreHTTPSErrors: true,
             headless : false,
@@ -37,25 +37,28 @@ async function instagram_follower_crawler(id, password) {
             response => response.url() === 'https://www.instagram.com/accounts/onetap/?next=%2F' && 
                         response.status() === 200
         );
-        await page.focus('button[class=".yWX7d"]');
-        console.log('0');
+        //await page.focus('button[class=".yWX7d"]');
         //await page.click('button[class="sqdOP.yWX7d.y3zKF"]');
-        console.log('1')
 
-        await document.querySelector(".sqdOP.yWX7d.y3zKF").click();
-        console.log('2')
+        await page.goto(instagram_url);
+        await page.waitForSelector('a[class="gmFkV"]');
+        await page.click('a[class="gmFkV"]');
 
+        await page.waitForSelector('a[class="-nal3 "]');
+        await page.click('a[class="-nal3 "]');
 
-        console.log('test');
-        await page.waitForResponse(
-            response => 
-                response.url() === 'https://www.instagram.com/' &&
-                response.status() === 200);
-        console.log('test1');
+        const follower_list = await page.evaluate(()=>{
+            let user_list = document.querySelector(".PZuss");
+            console.log(user_list);
 
+            return {
+                user_list,
+            };
+        });
+
+        console.log(follower_list);
         
         
-
     } catch(e) {
         console.log(e);
     }
