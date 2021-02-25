4b589bf7a4658d2cb55382ab33b675d774ef3001
evanha <bc.0x4861@gmail.com>
2021-02-26 00:28:41 +0900

    [add] code modifications

--- crawler/src/js/instagram_follower.js
+++ crawler/src/js/instagram_follower.js
@@ -7,7 +7,7 @@ const root_path = path.join(__dirname, '../../');
 const env_path = path.join(root_path, '.env');
 let env = dotenv.config({path: env_path});
 
-async function instagram_follower_crawler(id, password) {
+const instagram_follower_crawler = async (id, password) => {
     try {
         //console.log('root_path : '+ root_path);
         const browser = await puppeteer.launch({
@@ -21,8 +21,29 @@ async function instagram_follower_crawler(id, password) {
         page.setViewport({
             width: 1080,
             height: 1080,
-          });
-        await page.goto(instagram_url);
+        });
+
+        // Define a window.onCustomEvent function on the page.
+        await page.exposeFunction('onCustomEvent', (e) => {
+            console.log(`${e.type} fired`, e.detail || '');
+        });
+
+        /**
+        * Attach an event listener to page to capture a custom event on page load/navigation.
+        * @param {string} type Event name.
+        * @returns {!Promise}
+        */
+        function listenFor(type) {
+            return page.evaluateOnNewDocument((type) => {
+            document.addEventListener(type, (e) => {
+                window.onCustomEvent({ type, detail: e.detail });
+            });
+            }, type);
+        }
+
+        await listenFor('app-ready'); // Listen for "app-ready" custom event on page load.
+
+        await page.goto(instagram_url, {waitUntil: 'networkidle2'});
         await page.waitForSelector('input[name="username"]');
         
         await page.focus('input[name="username"]');
@@ -37,28 +58,25 @@ async function instagram_follower_crawler(id, password) {
             response => response.url() === 'https://www.instagram.com/accounts/onetap/?next=%2F' && 
                         response.status() === 200
         );
-        //await page.focus('button[class=".yWX7d"]');
-        //await page.click('button[class="sqdOP.yWX7d.y3zKF"]');
 
-        await page.goto(instagram_url);
+        await page.goto(instagram_url, {waitUntil: 'networkidle2'});
+
         await page.waitForSelector('a[class="gmFkV"]');
         await page.click('a[class="gmFkV"]');
+
+        await page.waitForSelector('span[class="g47SY "]');
+        const follower_count = await page.waitForSelector('span[class="g47SY "]').value;
+        console.log('follower_count : '+follower_count);
 
         await page.waitForSelector('a[class="-nal3 "]');
         await page.click('a[class="-nal3 "]');
 
-        const follower_list = await page.evaluate(()=>{
-            let user_list = document.querySelector(".PZuss");
-            console.log(user_list);
-
-            return {
-                user_list,
-            };
-        });
-
-        console.log(follower_list);
         
-        
+        let user_list = document.querySelector(".PZuss");
+        console.log(user_list);
+
+
+        //browser.close();
     } catch(e) {
         console.log(e);
     }
